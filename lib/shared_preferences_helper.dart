1a90d40f671048dacaa8fb9f8bda73ccc42ebfb7
Martin Labanic <martin.labanic@gmail.com>
2020-04-27 21:00:34 -0700

    Added color preferences to the shared preferences.

--- lib/shared_preferences_helper.dart
+++ lib/shared_preferences_helper.dart
@@ -1,3 +1,5 @@
+import "dart:ui";
+
 import "package:shared_preferences/shared_preferences.dart";
 
 const String USER_PREF_MAX_NUMBER_OF_DICE = "max_number_of_dice";
@@ -6,8 +8,20 @@ const String USER_PREF_RESET_NUMBER_OF_DICE_AFTER_ROLL = "reset_number_of_dice_a
 const String USER_PREF_RESET_MODIFIER_AFTER_ADD = "reset_modifier_after_add";
 const String USER_PREF_RESET_NUMBER_OF_DICE_AFTER_ADD = "reset_number_of_dice_after_add";
 const String USER_PREF_LANDSCAPE_CALC_ON_RIGHT = "landscape_calc_on_right";
-//const String USER_PREF_ = "";
+const String USER_PREF_PRIMARY_COLOR = "primary_colour";
+const String USER_PREF_PRIMARY_COLOR_DEFAULT = "0xFFEEEEEE";
+const String USER_PREF_SECONDARY_COLOR = "secondary_colour";
+const String USER_PREF_SECONDARY_COLOR_DEFAULT = "0xFFEEEEEE";
+const String USER_PREF_TERTIARY_COLOR = "tertiary_colour";
+const String USER_PREF_TERTIARY_COLOR_DEFAULT = "0xFF000000";
+const String USER_PREF_DICE_ROLLED_TEXT_COLOR = "_dice_rolled_text_colour";
+const String USER_PREF_DICE_ROLLED_TEXT_COLOR_DEFAULT = "0xFF03A9F4";
+const String USER_PREF_RESULT_TEXT_COLOR = "result_text_colour";
+const String USER_PREF_RESULT_TEXT_COLOR_DEFAULT = "0xFF000000";
+const String USER_PREF_RESULT_DETAILS_TEXT_COLOR = "detailed_result_text_colour";
+const String USER_PREF_RESULT_DETAILS_TEXT_COLOR_DEFAULT = "0x73000000";
 
+/// Implementation note: Not using the get / set keywords as you can't use async.
 class SharedPreferencesHelper {
   ///
   static Future<int> getMaxNumberOfDice() async {
@@ -15,7 +29,7 @@ class SharedPreferencesHelper {
     var result = prefs.getInt(USER_PREF_MAX_NUMBER_OF_DICE);
     if (result == null) {
       result = 12;
-      prefs.setInt(USER_PREF_MAX_NUMBER_OF_DICE, result);
+      await prefs.setInt(USER_PREF_MAX_NUMBER_OF_DICE, result);
     }
 
     return result;
@@ -27,7 +41,7 @@ class SharedPreferencesHelper {
     var result = prefs.getBool(USER_PREF_RESET_MODIFIER_AFTER_ROLL);
     if (result == null) {
       result = true;
-      prefs.setBool(USER_PREF_RESET_MODIFIER_AFTER_ROLL, result);
+      await prefs.setBool(USER_PREF_RESET_MODIFIER_AFTER_ROLL, result);
     }
 
     return result;
@@ -39,7 +53,7 @@ class SharedPreferencesHelper {
     var result = prefs.getBool(USER_PREF_RESET_NUMBER_OF_DICE_AFTER_ROLL);
     if (result == null) {
       result = true;
-      prefs.setBool(USER_PREF_RESET_NUMBER_OF_DICE_AFTER_ROLL, result);
+      await prefs.setBool(USER_PREF_RESET_NUMBER_OF_DICE_AFTER_ROLL, result);
     }
 
     return result;
@@ -51,7 +65,7 @@ class SharedPreferencesHelper {
     var result = prefs.getBool(USER_PREF_RESET_MODIFIER_AFTER_ADD);
     if (result == null) {
       result = true;
-      prefs.setBool(USER_PREF_RESET_MODIFIER_AFTER_ADD, result);
+      await prefs.setBool(USER_PREF_RESET_MODIFIER_AFTER_ADD, result);
     }
 
     return result;
@@ -63,7 +77,7 @@ class SharedPreferencesHelper {
     var result = prefs.getBool(USER_PREF_RESET_NUMBER_OF_DICE_AFTER_ADD);
     if (result == null) {
       result = true;
-      prefs.setBool(USER_PREF_RESET_NUMBER_OF_DICE_AFTER_ADD, result);
+      await prefs.setBool(USER_PREF_RESET_NUMBER_OF_DICE_AFTER_ADD, result);
     }
 
     return result;
@@ -75,7 +89,52 @@ class SharedPreferencesHelper {
     var result = prefs.getBool(USER_PREF_LANDSCAPE_CALC_ON_RIGHT);
     if (result == null) {
       result = true;
-      prefs.setBool(USER_PREF_LANDSCAPE_CALC_ON_RIGHT, result);
+      await prefs.setBool(USER_PREF_LANDSCAPE_CALC_ON_RIGHT, result);
+    }
+
+    return result;
+  }
+
+  ///
+  static Future<Color> getPrimaryColor() async {
+    return await _getColor(USER_PREF_PRIMARY_COLOR, USER_PREF_PRIMARY_COLOR_DEFAULT);
+  }
+
+  ///
+  static Future<Color> getSecondaryColor() async {
+    return await _getColor(USER_PREF_SECONDARY_COLOR, USER_PREF_SECONDARY_COLOR_DEFAULT);
+  }
+
+  ///
+  static Future<Color> getTertiaryColor() async {
+    return await _getColor(USER_PREF_TERTIARY_COLOR, USER_PREF_TERTIARY_COLOR_DEFAULT);
+  }
+
+  ///
+  static Future<Color> getDiceRolledTextColor() async {
+    return await _getColor(USER_PREF_DICE_ROLLED_TEXT_COLOR, USER_PREF_DICE_ROLLED_TEXT_COLOR_DEFAULT);
+  }
+
+  ///
+  static Future<Color> getResultTextColor() async {
+    return await _getColor(USER_PREF_RESULT_TEXT_COLOR, USER_PREF_RESULT_TEXT_COLOR_DEFAULT);
+  }
+
+  ///
+  static Future<Color> getResultDetailsTextColor() async {
+    return await _getColor(USER_PREF_RESULT_DETAILS_TEXT_COLOR, USER_PREF_RESULT_DETAILS_TEXT_COLOR_DEFAULT);
+  }
+
+  ///
+  static Future<Color> _getColor(String colourName, String defaultColor) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    Color result;
+    var hexColor = prefs.getString(colourName);
+    if (hexColor != null) {
+      result = Color(int.parse(hexColor));
+    } else {
+      result = Color(int.parse(defaultColor));
+      await prefs.setString(colourName, defaultColor);
     }
 
     return result;
@@ -114,5 +173,41 @@ class SharedPreferencesHelper {
   static Future<bool> setLandscapeCalcOnRight(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return value != null ? await prefs.setBool(USER_PREF_LANDSCAPE_CALC_ON_RIGHT, value) : false;
+  }
+
+  ///
+  static Future<bool> setPrimaryColor(Color value) async {
+    return await _setColor(USER_PREF_PRIMARY_COLOR, value);
+  }
+
+  ///
+  static Future<bool> setSecondaryColor(Color value) async {
+    return await _setColor(USER_PREF_SECONDARY_COLOR, value);
+  }
+
+  ///
+  static Future<bool> setTertiaryColor(Color value) async {
+    return await _setColor(USER_PREF_TERTIARY_COLOR, value);
+  }
+
+  ///
+  static Future<bool> setDiceRolledTextColor(Color value) async {
+    return await _setColor(USER_PREF_DICE_ROLLED_TEXT_COLOR, value);
+  }
+
+  ///
+  static Future<bool> setResultTextColor(Color value) async {
+    return await _setColor(USER_PREF_RESULT_TEXT_COLOR, value);
+  }
+
+  ///
+  static Future<bool> setDetailedResultTextColor(Color value) async {
+    return await _setColor(USER_PREF_RESULT_DETAILS_TEXT_COLOR, value);
+  }
+
+  ///
+  static Future<bool> _setColor(String name, Color value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return value != null ? await prefs.setString(name, value.toString()) : false;
   }
 }
